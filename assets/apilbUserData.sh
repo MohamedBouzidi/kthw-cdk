97dfd03c703cd3b9337f33281e9724acc952bbed
MohamedBouzidi <mohamedbouzidi95@hotmail.com>
2021-06-03 17:19:04 +0000

    update

---
+++ assets/apilbUserData.sh
@@ -0,0 +1,256 @@
+#!/bin/bash
+
+## Replace KEY_NAME
+## Replace BUCKET_NAME
+## Replace WORKERS
+## Replace CONTROLLERS
+## Replace CONTROLLERIPS
+## Replace NGINXBACKEND
+
+## Set Workdir
+cd /home/ubuntu
+
+## Install AWSCLI
+sudo apt-get update -y
+sudo apt-get install -y unzip
+curl -o "awscliv2.zip" "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
+unzip awscliv2.zip > /dev/null
+sudo ./aws/install
+
+## Download Key
+aws s3 cp s3://BUCKET_NAME/KEY_NAME KEY_NAME
+chmod 400 KEY_NAME
+
+## Download tools
+wget -q --timestamping \
+        https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssl \
+        https://storage.googleapis.com/kubernetes-the-hard-way/cfssl/1.4.1/linux/cfssljson
+        chmod +x cfssl cfssljson
+        sudo mv cfssl cfssljson /usr/local/bin/
+        wget https://storage.googleapis.com/kubernetes-release/release/v1.21.0/bin/linux/amd64/kubectl
+        chmod +x kubectl
+        sudo mv kubectl /usr/local/bin/
+
+## Generate SSL
+cat > ca-config.json <<EOF
+{
+  "signing": {
+    "default": {
+      "expiry": "8760h"
+    },
+    "profiles": {
+      "kubernetes": {
+        "usages": ["signing", "key encipherment", "server auth", "client auth"],
+        "expiry": "8760h"
+      }
+    }
+  }
+}
+EOF
+
+cat > ca-csr.json <<EOF
+{
+  "CN": "Kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+},
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "CA",
+      "ST": "Oregon"
+    }
+  ]
+}
+EOF
+
+cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+cat > kubernetes-csr.json <<EOF
+{
+  "CN": "kubernetes",
+  "key": {
+    "algo": "rsa",
+    "size": 2048
+},
+  "names": [
+    {
+      "C": "US",
+      "L": "Portland",
+      "O": "Kubernetes",
+      "OU": "Kubernetes The Hard Way",
+      "ST": "Oregon"
+}
+  ]
+}
+EOF
+
+API_LB=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+KUBERNETES_HOSTNAMES=kubernetes,kubernetes.default,kubernetes.default.svc,kubernetes.default.svc.cluster,kubernetes.svc.cluster.local
+
+cfssl gencert \
+        -ca=ca.pem \
+        -ca-key=ca-key.pem \
+        -config=ca-config.json \
+        -hostname=10.32.0.1,CONTROLLERIPS,${API_LB},127.0.0.1,${KUBERNETES_HOSTNAMES} \
+        -profile=kubernetes \
+        kubernetes-csr.json | cfssljson -bare kubernetes
+
+## create encryption config
+ENCRYPTION_KEY=$(head -c 32 /dev/urandom | base64)
+
+cat > encryption-config.yaml <<EOF
+kind: EncryptionConfig
+apiVersion: v1
+resources:
+  - resources:
+      - secrets
+    providers:
+      - aescbc:
+          keys:
+            - name: key1
+              secret: ${ENCRYPTION_KEY}
+      - identity: {}
+EOF
+
+## configure controllers
+for instance in CONTROLLERS; do
+        name=$(echo ${instance} | cut -d= -f1)
+        address=$(echo ${instance} | cut -d= -f2)
+        scp -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ca.pem ca-key.pem ca-config.json \
+                kubernetes.pem kubernetes-key.pem encryption-config.yaml ubuntu@${address}:~/
+                        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/generateSSL.sh
+                        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/generateKubeconfig.sh
+                        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/installEtcd.sh
+                        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/startServices.sh
+                done
+
+                controller0=$(echo CONTROLLERS | cut -d' ' -f1 | cut -d= -f2)
+                ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@$controller0 /home/ubuntu/enableKubeletAuth.sh
+
+              ## configure api load balancer
+              sudo apt-get update -y
+              sudo apt-get install -y nginx
+              # in the nginx config, use kubernetes.pem instead of ca.pem
+              cat > /etc/nginx/nginx.conf <<EOF
+user www-data;
+worker_processes auto;
+pid /run/nginx.pid;
+include /etc/nginx/modules-enabled/*.conf;
+
+events {
+        worker_connections 768;
+        # multi_accept on;
+}
+
+http {
+
+        upstream apiserver {
+                NGINXBACKEND
+        }
+
+        server {
+                listen 6443 ssl;
+                server_name kubernetes.default.svc.cluster.local;
+
+                ssl_certificate         /home/ubuntu/kubernetes.pem;
+                ssl_certificate_key     /home/ubuntu/kubernetes-key.pem;
+
+                location / {
+                        proxy_pass                      https://apiserver;
+                        proxy_ssl_certificate           /home/ubuntu/ca.pem;
+                        proxy_ssl_certificate_key       /home/ubuntu/ca-key.pem;
+                }
+        }
+
+        ##
+        # Basic Settings
+        ##
+
+        sendfile on;
+        tcp_nopush on;
+        tcp_nodelay on;
+        keepalive_timeout 65;
+        types_hash_max_size 2048;
+        # server_tokens off;
+
+        # server_names_hash_bucket_size 64;
+        # server_name_in_redirect off;
+
+        include /etc/nginx/mime.types;
+        default_type application/octet-stream;
+
+        ##
+        # SSL Settings
+        ##
+
+        ssl_protocols TLSv1 TLSv1.1 TLSv1.2 TLSv1.3; # Dropping SSLv3, ref: POODLE
+        ssl_prefer_server_ciphers on;
+
+        ##
+        # Logging Settings
+        ##
+
+        access_log /var/log/nginx/access.log;
+        error_log /var/log/nginx/error.log;
+
+        ##
+        # Gzip Settings
+        ##
+
+        gzip on;
+
+        # gzip_vary on;
+        # gzip_proxied any;
+        # gzip_comp_level 6;
+        # gzip_buffers 16 8k;
+        # gzip_http_version 1.1;
+        # gzip_types text/plain text/css application/json application/javascript text/xml application/xml application/xml+rss text/javascript;
+
+        ##
+        # Virtual Host Configs
+        ##
+
+        include /etc/nginx/conf.d/*.conf;
+        include /etc/nginx/sites-enabled/*;
+}
+
+
+#mail {
+#       # See sample authentication script at:
+#       # http://wiki.nginx.org/ImapAuthenticateWithApachePhpScript
+#
+#       # auth_http localhost/auth.php;
+#       # pop3_capabilities "TOP" "USER";
+#       # imap_capabilities "IMAP4rev1" "UIDPLUS";
+#
+#       server {
+#               listen     localhost:110;
+#               protocol   pop3;
+#               proxy      on;
+#       }
+#
+#       server {
+#               listen     localhost:143;
+#               protocol   imap;
+#               proxy      on;
+#       }
+#}
+EOF
+nginx -t
+systemctl start nginx
+systemctl enable nginx
+
+## configure workers
+for instance in WORKERS; do
+        name=$(echo ${instance} | cut -d= -f1)
+        address=$(echo ${instance} | cut -d= -f2)
+        scp -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ca.pem ca-key.pem ca-config.json ubuntu@${address}:~/
+        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/generateSSL.sh
+        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/generateKubeconfig.sh
+        ssh -i /home/ubuntu/KEY_NAME -o StrictHostKeyChecking=no ubuntu@${address} /home/ubuntu/startServices.sh
+done
+
--- lib/kthw-stack.ts
+++ lib/kthw-stack.ts
@@ -1,9 +1,253 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as iam from '@aws-cdk/aws-iam';
+import { readFileSync } from 'fs';
+import * as path from 'path';
 
 export class KthwStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+	constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+		const keyName = 'kthw';
+		const bucketName = 'magictraining-bucket';
+		const apilbName = 'api-load-balancer';
+		const numberOfWorkers = 1; // max 9
+		const numberOfControllers = 1; // max 9
+
+		const vpc = new ec2.Vpc(this, 'Vpc', {
+			maxAzs: 2,
+			cidr: '10.240.0.0/16',
+			subnetConfiguration: [
+				{
+					cidrMask: 24,
+					name: 'Default',
+					subnetType: ec2.SubnetType.PUBLIC,
+				},
+			],
+		});
+
+		const AMI =
+			'/aws/service/canonical/ubuntu/server/20.04/stable/current/amd64/hvm/ebs-gp2/ami-id';
+		const machineImage = ec2.MachineImage.fromSSMParameter(
+			AMI,
+			ec2.OperatingSystemType.LINUX
+		);
+		const controllerInstanceType = ec2.InstanceType.of(
+			ec2.InstanceClass.BURSTABLE2,
+			ec2.InstanceSize.MICRO
+		);
+		const workerInstanceType = ec2.InstanceType.of(
+			ec2.InstanceClass.BURSTABLE2,
+			ec2.InstanceSize.MICRO
+		);
+
+		const clusterSecurityGroup = new ec2.SecurityGroup(this, 'clusterSG', {
+			vpc: vpc,
+			allowAllOutbound: true,
+			securityGroupName: 'clusterSG',
+		});
+		clusterSecurityGroup.addIngressRule(
+			ec2.Peer.anyIpv4(),
+			ec2.Port.tcp(22),
+			'Allow external ssh'
+		);
+		clusterSecurityGroup.addIngressRule(
+			ec2.Peer.anyIpv4(),
+			ec2.Port.tcp(6443),
+			'Allow external api'
+		);
+		clusterSecurityGroup.addIngressRule(
+			ec2.Peer.anyIpv4(),
+			ec2.Port.tcp(80),
+			'Allow external api'
+		);
+		clusterSecurityGroup.addIngressRule(
+			ec2.Peer.anyIpv4(),
+			ec2.Port.allIcmp(),
+			'Allow external icmp'
+		);
+
+		const internalNetworks = ['10.240.0.0/24', '10.200.0.0/16'];
+		for (let i = 0; i < internalNetworks.length; i++) {
+			clusterSecurityGroup.addIngressRule(
+				ec2.Peer.ipv4(internalNetworks[i]),
+				ec2.Port.allTcp(),
+				'Allow internal tcp'
+			);
+			clusterSecurityGroup.addIngressRule(
+				ec2.Peer.ipv4(internalNetworks[i]),
+				ec2.Port.allUdp(),
+				'Allow internal udp'
+			);
+			clusterSecurityGroup.addIngressRule(
+				ec2.Peer.ipv4(internalNetworks[i]),
+				ec2.Port.allIcmp(),
+				'Allow internal icmp'
+			);
+		}
+
+		const albRole = new iam.Role(this, 'albrole', {
+			assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+			inlinePolicies: {
+				SSHKeyBucket: new iam.PolicyDocument({
+					assignSids: true,
+					statements: [
+						new iam.PolicyStatement({
+							effect: iam.Effect.ALLOW,
+							actions: ['s3:GetObject'],
+							resources: [`arn:aws:s3:::${bucketName}/${keyName}.pem`],
+						}),
+					],
+				}),
+			},
+		});
+
+		let apilbUserData = readFileSync(
+			path.join(__dirname, '../assets/apilbUserData.sh'),
+			'utf8'
+		);
+		apilbUserData = apilbUserData.replace(/BUCKET_NAME/g, bucketName);
+		apilbUserData = apilbUserData.replace(/KEY_NAME/g, `${keyName}.pem`);
+		apilbUserData = apilbUserData.replace(
+			/WORKERS/g,
+			new Array(numberOfWorkers)
+			.fill(0)
+			.map((_, i) => `worker${i}=10.240.0.2${i}`)
+			.join(' ')
+		);
+		apilbUserData = apilbUserData.replace(
+			/CONTROLLERS/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => `controller${i}=10.240.0.1${i}`)
+			.join(' ')
+		);
+		apilbUserData = apilbUserData.replace(
+			/CONTROLLERIPS/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => '10.240.0.1' + i)
+			.join(',')
+		);
+		apilbUserData = apilbUserData.replace(
+			/NGINXBACKEND/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => `server 10.240.0.1${i}:6443;`)
+			.join('\n                ')
+		);
+
+		const apilb = new ec2.Instance(this, apilbName, {
+			instanceName: apilbName,
+			instanceType: controllerInstanceType,
+			machineImage: machineImage,
+			securityGroup: clusterSecurityGroup,
+			vpcSubnets: { subnets: [vpc.publicSubnets[0]] },
+			userData: ec2.UserData.custom(apilbUserData),
+			sourceDestCheck: true,
+			keyName: keyName,
+			role: albRole,
+			vpc: vpc,
+		});
+
+		const kubeRole = new iam.Role(this, 'clusterrole', {
+			assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+			inlinePolicies: {
+				SSHKeyBucket: new iam.PolicyDocument({
+					assignSids: true,
+					statements: [
+						new iam.PolicyStatement({
+							effect: iam.Effect.ALLOW,
+							actions: [
+								'ec2:DescribeInstances',
+								'ec2:DescribeImages',
+								'ec2:DescribeTags'
+							],
+							resources: ['*'],
+						}),
+					],
+				}),
+			},
+		});
+
+		let controllerUserData = readFileSync(path.join(__dirname, '../assets/controllerUserData.sh'), 'utf8');
+		controllerUserData = controllerUserData.replace(/APILBNAME/g, apilbName);
+		controllerUserData = controllerUserData.replace(
+			/CONTROLLERIPS/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => '10.240.0.1' + i)
+			.join(',')
+		);
+		controllerUserData = controllerUserData.replace(
+			/ETCDINITIAL/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => `controller${i}=https://10.240.0.1${i}:2380`)
+				.join(',')
+		);
+		controllerUserData = controllerUserData.replace(
+			/ETCDCLUSTER/g,
+			new Array(numberOfControllers)
+			.fill(0)
+			.map((_, i) => `https://10.240.0.1${i}:2379`)
+				.join(',')
+		);
+
+		const controllers = [];
+		for (let i = 0; i < numberOfControllers; i++) {
+			const name = 'controller' + i;
+			const localip = '10.240.0.1' + i;
+			let userData = controllerUserData.slice();
+			userData = userData.replace(/LOCALIP/g, localip);
+			userData = userData.replace(/INSTANCE/g, name);
+			controllers.push(
+				new ec2.Instance(this, name, {
+					instanceName: name,
+					instanceType: controllerInstanceType,
+					machineImage: machineImage,
+					securityGroup: clusterSecurityGroup,
+					privateIpAddress: localip,
+					userData: ec2.UserData.custom(userData),
+					vpcSubnets: { subnets: [vpc.publicSubnets[0]] },
+					sourceDestCheck: true,
+					keyName: keyName,
+					role: kubeRole,
+					vpc: vpc,
+				})
+			);
+			apilb.node.addDependency(controllers[0]);
+		}
+
+		let workerUserData = readFileSync(path.join(__dirname, '../assets/workerUserData.sh'), 'utf8');
+		workerUserData = workerUserData.replace(/APILBNAME/g, apilbName);
+
+		const workers = [];
+		for (let i = 0; i < numberOfWorkers; i++) {
+			const name = 'worker' + i;
+			const localip = '10.240.0.2' + i;
+			const podcidr = '10.200.' + i + '.0/24';
+			let userData = workerUserData.slice();
+			userData = userData.replace(/PODCIDR/g, podcidr);
+			userData = userData.replace(/LOCALIP/g, localip);
+			userData = userData.replace(/INSTANCE/g, name);
+			workers.push(
+				new ec2.Instance(this, name, {
+					instanceName: name,
+					instanceType: workerInstanceType,
+					machineImage: machineImage,
+					securityGroup: clusterSecurityGroup,
+					privateIpAddress: localip,
+					userData: ec2.UserData.custom(userData),
+					vpcSubnets: { subnets: [vpc.publicSubnets[0]] },
+					sourceDestCheck: true,
+					keyName: keyName,
+					role: kubeRole,
+					vpc: vpc,
+				})
+			);
+			apilb.node.addDependency(workers[0]);
+		}
+	}
 }
+
